ca33696466b082de08c2781fc5b6adf5a7282ef8
MrJa777 <684244132@mail.pbru.ac.th>
2025-07-01 11:57:22 +0700

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+// s 
+  string s = "           ";
+   cout << endl << endl << "\n\n\n\n";
+  cout << "    **    ** " << endl;
+  cout << "  ************ " << endl;
+  cout << "   ********* " << endl;
+  cout << "     ****** " << endl;
+  cout << "       ** ";
+          
+  return 0;
+}
